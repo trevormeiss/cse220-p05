ed0a662ddf1502219350692a8dbae373b1a8011e
Trevor Meiss <tmeiss@Trevor-Meisss-MacBook-Pro.local>
2013-04-29 01:33:34 -0700

    Initial Commit

---
+++ Project 5 Final/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Project 5 Final
+//
+//  Created by Trevor Meiss on 4/29/13.
+//  Copyright (c) 2013 Trevor Meiss. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
